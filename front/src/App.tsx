cebab2cfedc9d2a81a42e36cfe983bb4501de137
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-08-24 20:15:42 +0300

    FRONT: setup apollo client

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 import "fontsource-roboto";
+
+import { apolloClient } from "./graphql/apollo";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Hello mr school
-      </Button>
-    </div>
+    <Router>
+      <ApolloProvider client={apolloClient}>
+        <Button variant="contained" color="primary">
+          Hello mr school
+        </Button>
+      </ApolloProvider>
+    </Router>
   );
 }
 
---
+++ front/src/configs/index.ts
@@ -0,0 +1,5 @@
+export const env = {
+  WS_ADRESSE: "ws://localhost:5000/graphql",
+  API_ADRESSE: "http://localhost:5000/graphql",
+  TOKEN_NAME: "%^#%@&fjlakjdlkfj1645165",
+};
