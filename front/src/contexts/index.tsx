138eb7775c33db49e3db9db040d697ca882592d6
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-08-24 22:01:52 +0300

    FRONT: ContextProvider Ctx => Provider

--- front/src/contexts/index.tsx
+++ front/src/contexts/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { SessionProvider } from "./session/session.context";
 
 export const ContextProvider: React.FC<any> = ({ children }) => {
-  return [SessionProvider].reduce((acc, Ctx) => {
-    return <Ctx>{acc}</Ctx>;
+  return [SessionProvider].reduce((acc, Provider) => {
+    return <Provider>{acc}</Provider>;
   }, children);
 };
