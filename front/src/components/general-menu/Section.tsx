a6724be8e4528511c85cd0a2d120b7b837c34c7a
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-08-25 10:54:02 +0300

    FRONT: remplassement arrow menu section

--- front/src/components/general-menu/Section.tsx
+++ front/src/components/general-menu/Section.tsx
@@ -1,11 +1,37 @@
 import * as React from "react";
 import ScrollMenu from "react-horizontal-scrolling-menu";
-import { Typography, makeStyles, Box } from "@material-ui/core";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { Typography, makeStyles, IconButton } from "@material-ui/core";
 
 import { SectionItem } from "./SectionMenu";
 import SectionMenu from "./SectionMenu";
+
+import rightArrow from "../../assets/031-right-arrow-10.png";
+import leftArrow from "../../assets/047-left-arrow-10.png";
+
+const useArrowStyle = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
+
+const RightArrow = () => {
+  const classes = useArrowStyle();
+  return (
+    <IconButton>
+      <img className={classes.small} alt="righ arrow" src={rightArrow} />
+    </IconButton>
+  );
+};
+
+const LeftArrow = () => {
+  const classes = useArrowStyle();
+  return (
+    <IconButton>
+      <img className={classes.small} alt="left arrow" src={leftArrow} />
+    </IconButton>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -42,8 +68,8 @@ const Section: React.FC<SectionProps> = ({ sections }) => {
             data={section.sectionItems.map((sectionItem, i) => (
               <SectionMenu key={i} color={section.color} item={sectionItem} />
             ))}
-            arrowLeft={<ArrowBackIosIcon />}
-            arrowRight={<ArrowForwardIosIcon />}
+            arrowLeft={<LeftArrow />}
+            arrowRight={<RightArrow />}
             alignCenter={false}
             itemStyle={{ marginRight: 25 }}
             menuStyle={{ marginBottom: 25 }}
