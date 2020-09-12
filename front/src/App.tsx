2bfeedacfb70f73c721d53de03d73e34e2342c8c
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-12 16:24:45 +0300

    feat(front): remise en place enscient front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import 'fontsource-roboto';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import "fontsource-roboto";
 
-import { ContextProvider } from './contexts';
-import { apolloClient } from './graphql/apollo';
+import { ContextProvider } from "./contexts";
+import { apolloClient } from "./graphql/apollo";
 
 function App() {
   return (
     <Router>
       <head>
-        <link rel="stylesheet" href={require('hover.css/css/hover-min.css')} />
+        <link rel="stylesheet" href={require("hover.css/css/hover-min.css")} />
       </head>
 
       <ContextProvider>
         <ApolloProvider client={apolloClient}>
-          <Route path="/login" component={require('./pages/Login').default} />
-          <Route path="/home" component={require('./pages/Home').default} />
-          <Route path="/main" component={require('./pages/Main').default} />
-          <></>
+          <Switch>
+            <Route path="/login" component={require("./pages/Login").default} />
+            <Route path="/home" component={require("./pages/Home").default} />
+            <Route path="/main" component={require("./pages/Main").default} />
+          </Switch>
         </ApolloProvider>
       </ContextProvider>
     </Router>
