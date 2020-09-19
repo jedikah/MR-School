ed7682de024fcff61960993aab1541dc05d2a41d
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-19 18:03:35 +0300

    fix(front): create eleve

--- front/src/components/main/gestion-eleve/liste-eleve/Eleves.tsx
+++ front/src/components/main/gestion-eleve/liste-eleve/Eleves.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   makeStyles,
   withStyles,
   createStyles,
-  Theme
-} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+  Theme,
+} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import {
   Toolbar,
   Typography,
@@ -21,45 +21,45 @@ import {
   IconButton,
   Button,
   ButtonGroup,
-  TablePagination
-} from '@material-ui/core';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+  TablePagination,
+} from "@material-ui/core";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
-import user from '../../../../assets/user.png';
-import { TableRowSkeleton } from '../../../public-component/TableSkeleton';
-import { CreateData, generateDataTable } from './DataCreator';
-import { UseEleves } from '../../../../graphql/eleve/eleves/eleves.service';
+import user from "../../../../assets/user.png";
+import { TableRowSkeleton } from "../../../public-component/TableSkeleton";
+import { CreateData, generateDataTable } from "./DataCreator";
+import { UseEleves } from "../../../../graphql/eleve/eleves/eleves.service";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      '& > *': {
-        borderBottom: 'unset'
-      }
+      "& > *": {
+        borderBottom: "unset",
+      },
     },
     rootPagination: {
-      width: '100%'
+      width: "100%",
     },
     container: {
-      height: '80vh',
-      maxHeight: '85vh'
+      height: "80vh",
+      maxHeight: "85vh",
     },
     table: {
       minWidth: 650,
-      boxShadow: 'null'
+      boxShadow: "null",
     },
     toolbar: {
       backgroundColor: theme.palette.secondary.main,
-      color: 'white'
+      color: "white",
     },
     center: {
-      marginLeft: 'auto',
-      marginRight: 'auto'
+      marginLeft: "auto",
+      marginRight: "auto",
     },
     tbColapsTitle: {
-      fontWeight: 'bold'
-    }
+      fontWeight: "bold",
+    },
   })
 );
 
@@ -67,11 +67,11 @@ const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
     head: {
       backgroundColor: theme.palette.secondary.light,
-      color: 'white'
+      color: "white",
     },
     body: {
-      fontSize: 14
-    }
+      fontSize: 14,
+    },
   })
 )(TableCell);
 
@@ -95,7 +95,7 @@ const Row: React.FC<{ row: CreateData }> = (props) => {
         <TableCell component="th" scope="row">
           <Avatar
             sizes="small"
-            alt={'user'}
+            alt={"user"}
             src={user}
             className={classes.center}
           />
@@ -287,6 +287,12 @@ const Eleves: React.FC<UseEleves> = () => {
 
   let [rows, setRows] = React.useState<CreateData[]>([]);
 
+  React.useEffect(() => {
+    setTimeout(() => {
+      setRows(generateDataTable(50));
+    }, 3000);
+  }, []);
+
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const [page, setPage] = React.useState(0);
@@ -305,17 +311,13 @@ const Eleves: React.FC<UseEleves> = () => {
     setPage(0);
   };
 
-  setTimeout(() => {
-    setRows(generateDataTable(50));
-  }, 3000);
-
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container} component={Paper}>
         <Toolbar variant="dense" className={classes.toolbar}>
           <Typography
             variant="h6"
-            className={[classes.center, classes.tbColapsTitle].join(' ')}
+            className={[classes.center, classes.tbColapsTitle].join(" ")}
             id="tableTitle"
             component="div"
           >
