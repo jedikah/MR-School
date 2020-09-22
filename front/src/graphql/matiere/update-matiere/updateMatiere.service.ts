73af96bfb9167bdde73da9b732eafcf1fcd41bcc
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-22 17:49:56 +0300

    feat(front): update matiere

--- front/src/graphql/matiere/update-matiere/updateMatiere.service.ts
+++ front/src/graphql/matiere/update-matiere/updateMatiere.service.ts
@@ -4,6 +4,7 @@ import { UpdateMatiereData, UPDATE_MATIERE } from "./updateMatiere.gql";
 import { MutationUpdateMatiereArgs } from "../../types";
 import { useMatiereDispatch, useMatiereState } from "../matiere.consumer";
 import { MatiereState, MatiereDispatch } from "../matiere.context";
+import { useSnackbar } from "notistack";
 
 export interface UseUpdateMatiere {
   matiereState: MatiereState;
@@ -13,12 +14,36 @@ export interface UseUpdateMatiere {
 }
 
 export const useUpdateMatiere = () => {
+  const { enqueueSnackbar } = useSnackbar();
   const matiereState = useMatiereState();
   const matiereDispatch = useMatiereDispatch();
   const [updateMatiere, { loading: loadingUpdateMatiere }] = useMutation<
     UpdateMatiereData,
     MutationUpdateMatiereArgs
-  >(UPDATE_MATIERE);
+  >(UPDATE_MATIERE, {
+    onCompleted: (data) => {
+      enqueueSnackbar(
+        `La matiere a bien ete modifier en ${data.updateMatiere.designation}`,
+        {
+          variant: "success",
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "center",
+          },
+        }
+      );
+    },
+
+    onError: (error) => {
+      enqueueSnackbar(error.message, {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+      });
+    },
+  });
 
   const submitUpdateMatiere = () => {
     const variables = matiereState.updateMatiereVariables;
