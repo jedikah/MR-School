7951df91688058e87209edf71ff722b9947b6852
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-24 10:55:01 +0300

    feat(front): split table coefficient finished

--- front/src/components/main/gestion-ecole/matiere/table-matiere/TableCoefficient.tsx
+++ front/src/components/main/gestion-ecole/matiere/table-matiere/TableCoefficient.tsx
@@ -1,7 +1,111 @@
 import * as React from "react";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Checkbox,
+  TableHead,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+interface Data {
+  classe: string;
+  coefficient: number;
+}
+
+function createData(classe: string, coefficient: number): Data {
+  return { classe, coefficient };
+}
+
+const rows = [
+  createData("seconde", 2),
+  createData("premiere", 3),
+  createData("terminal", 4),
+];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    table: {
+      minWidth: 750,
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+    },
+
+    columText: {
+      fontWeight: "bold",
+    },
+  })
+);
 
 const TableCoefficient: React.FC = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Table
+      className={classes.table}
+      aria-labelledby="tableTitle"
+      size={"small"}
+      aria-label="enhanced table"
+    >
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox" scope="row">
+            <Checkbox checked={false} inputProps={{ "aria-labelledby": "0" }} />
+          </TableCell>
+
+          <TableCell scope="row" align="left" className={classes.columText}>
+            Classe
+          </TableCell>
+
+          <TableCell scope="row" align="center" className={classes.columText}>
+            Coefficient
+          </TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        {rows.map((row, index) => {
+          const isItemSelected = false;
+          const labelId = `enhanced-table-checkbox-${index}`;
+
+          return (
+            <TableRow
+              hover
+              onClick={(event) => {}}
+              role="checkbox"
+              aria-checked={isItemSelected}
+              tabIndex={-1}
+              key={row.classe}
+              selected={isItemSelected}
+            >
+              <TableCell padding="checkbox">
+                <Checkbox
+                  checked={isItemSelected}
+                  inputProps={{ "aria-labelledby": labelId }}
+                />
+              </TableCell>
+
+              <TableCell scope="row" align="left">
+                {row.classe}
+              </TableCell>
+
+              <TableCell scope="row" align="center">
+                {row.coefficient}
+              </TableCell>
+            </TableRow>
+          );
+        })}
+
+        <TableRow style={{ height: 33 }}>
+          <TableCell colSpan={6} />
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
 };
 
 export default TableCoefficient;
--- front/src/components/main/gestion-ecole/matiere/table-matiere/TableMatiere.tsx
+++ front/src/components/main/gestion-ecole/matiere/table-matiere/TableMatiere.tsx
@@ -1,50 +1,11 @@
 import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
-import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import Checkbox from "@material-ui/core/Checkbox";
 
 import TableToolbar from "./TableToolbar";
-import TableMatiereHead from "./TableMatiereHead";
 import { useMatieres } from "../../../../../graphql/matiere/matieres/matieres.service";
-
-interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
-}
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-): Data {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
+import TableCoefficient from "./TableCoefficient";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -65,54 +26,13 @@ const useStyles = makeStyles((theme: Theme) =>
       position: "relative",
       flexGrow: 1,
     },
-
-    table: {
-      minWidth: 750,
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      right: 0,
-      left: 0,
-    },
-
-    visuallyHidden: {
-      border: 0,
-      clip: "rect(0 0 0 0)",
-      height: 1,
-      margin: -1,
-      overflow: "hidden",
-      padding: 0,
-      position: "absolute",
-      top: 20,
-      width: 1,
-    },
   })
 );
 
 export default function EnhancedTable() {
   const classes = useStyles();
   const { matiereState } = useMatieres();
-  const [selected, setSelected] = React.useState<string[]>([]);
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
+  const [selected] = React.useState<string[]>([]);
 
   return (
     <div className={classes.root}>
@@ -124,55 +44,7 @@ export default function EnhancedTable() {
           }
         />
         <TableContainer className={classes.tableContainer}>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size={"small"}
-            aria-label="enhanced table"
-          >
-            <TableMatiereHead />
-            <TableBody>
-              {rows.map((row, index) => {
-                const isItemSelected = false;
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.name)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.name}
-                    selected={isItemSelected}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={isItemSelected}
-                        inputProps={{ "aria-labelledby": labelId }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                  </TableRow>
-                );
-              })}
-
-              <TableRow style={{ height: 33 }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            </TableBody>
-          </Table>
+          <TableCoefficient />
         </TableContainer>
       </Paper>
     </div>
